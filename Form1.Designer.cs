32687bab5f40ad0565f4e7cd6984a35a222d7197
Jonathan <576754@student.belgiumcampus.ac.za>
2021-10-04 13:41:57 +0200

    Added Controls to the Windows Form App

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,45 @@ namespace QuizPractical
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(13, 13);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(775, 324);
+            this.listBox.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(262, 368);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(285, 48);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read File";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnRead;
     }
 }
 
